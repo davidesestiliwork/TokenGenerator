a3a02d6061daf9f52620d952883e6c3248114f34
davide <davide.sestili@gmail.com>
2018-02-26 19:55:25 +0100

    Update

--- src/TokenGen.java
+++ src/TokenGen.java
@@ -1,6 +1,6 @@
 /*
 TokenGen a token generator
-Copyright (C) 2017 Davide Sestili
+Copyright (C) 2017-2018 Davide Sestili
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
